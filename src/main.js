59b9bdd3488df754016feea624d5cfcd793f2201
Hawa Abdi <abdih17@hotmail.com>
2017-07-18 23:31:29 -0700

    minor revision on main.js

--- src/main.js
+++ src/main.js
@@ -1,54 +1,49 @@
 import './style/main.scss';
+import faker from 'faker';
 import React from 'react';
 import ReactDom from 'react-dom';
+import cowsay from 'cowsay-browser';
 
 class Navbar extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  render(){
+  render() {
     return (
       <header className="navbar">
-        <h1> Generate Cowsay Lorem </h1>
+        <h1> Cowsay </h1>
       </header>
-    )
+    );
   }
 }
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      count: 0,
-      title: 'hello world'
-    }
+      content: 'cowsay'
+    };
 
-    this.lulwat = this.lulwat.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
-    console.log('event', e);
-
-    this.setState({count: Math.random()});
-    this.setState({oldState => {
-      return {
-        count: oldState.count + 3});
-      }
-    })
+    const content = faker.fake('{{lorem.sentence}}');
+    this.setState({ content });
   }
 
-  render(){
+  render() {
     return (
       <div>
         <Navbar />
-        <h1>Cool</h1>
-        <p onclick={this.handleClick}> counter: {this.state.count}</p>
-        <p>{this.state.title}</p>
+        <pre>
+          {cowsay.say({ text: this.state.content, e: 'oO', T: 'U' })}{' '}
+        </pre>
+        <button onClick={this.handleClick}>Click Me</button>
       </div>
-    )
+    );
   }
 }
 
-ReactDom.render(
-  <App />, document.getElementById('root'))
+ReactDom.render(<App />, document.getElementById('root'));
